d5b03e41e18412729db6a5b1aee931297ccaaff2
oceane monguillon <oceane.monguillon@gmail.com>
2021-08-12 14:35:26 +0200

    modif auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,17 +4,17 @@ const jwt = require('jsonwebtoken');
 //middleware qui permet de proteger les routes choisi et de verifier l'authentification
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
+    const token = req.headers.authorization.split(' ')[1];//récuperation du token 
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');//Décodage du token
+    const userId = decodedToken.userId; //decodage token de l'utilisateur
+    if (req.body.userId && req.body.userId !== userId) { 
+      throw 'id utilisateur invalide';
     } else {
       next();
     }
   } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
+    res.status(401).json({ //erreur pb d'authentification
+      error: new Error('requete invalide!')
     });
   }
 };
